1ed0cd9f092e9ccc78d47366379e3704579ed8fd
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-07-25 12:24:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj ={};
+	for(let  c of str){
+		if(obj[c]){
+			obj[c]=obj[c]+1;
+		}else{
+			obj[c]=1;
+		}
+	}
+	for(let c of obj){
+		if(obj[c]===1){
+			return c;
+		}
+	}
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
